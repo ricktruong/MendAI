d73becfc1e2c656477e1e15d330163a41a5d719c
MarlonMa17 <mahonghao1995@gmail.com>
2025-07-15 11:17:56 -0700

    Add comprehensive type declarations for various file formats in vite-env.d.ts

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,212 @@
 /// <reference types="vite/client" />
+
+// Image file type declaration
+declare module "*.svg" {
+  import React = require("react");
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
+
+declare module "*.png" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.jpg" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.jpeg" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.gif" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.webp" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.ico" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.bmp" {
+  const src: string;
+  export default src;
+}
+
+// Style file type declaration
+declare module "*.css" {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+declare module "*.scss" {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+declare module "*.sass" {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+declare module "*.less" {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+declare module "*.styl" {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+declare module "*.stylus" {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+// CSS Modules Type Declarations
+declare module "*.module.css" {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module "*.module.scss" {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module "*.module.sass" {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module "*.module.less" {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+// Font file type declaration
+declare module "*.woff" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.woff2" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.eot" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.ttf" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.otf" {
+  const src: string;
+  export default src;
+}
+
+// Audio and video file type declarations
+declare module "*.mp4" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.webm" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.ogg" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.mp3" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.wav" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.flac" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.aac" {
+  const src: string;
+  export default src;
+}
+
+// Document file type declaration
+declare module "*.pdf" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.txt" {
+  const src: string;
+  export default src;
+}
+
+// JSON file
+declare module "*.json" {
+  const content: any;
+  export default content;
+}
+
+// Web Workers
+declare module "*.worker.ts" {
+  const WorkerFactory: new () => Worker;
+  export default WorkerFactory;
+}
+
+declare module "*.worker.js" {
+  const WorkerFactory: new () => Worker;
+  export default WorkerFactory;
+}
+
+// Vite query parameters
+declare module "*?url" {
+  const src: string;
+  export default src;
+}
+
+declare module "*?raw" {
+  const src: string;
+  export default src;
+}
+
+declare module "*?inline" {
+  const src: string;
+  export default src;
+}
+
+// Environment variable type extension
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string;
+  readonly VITE_API_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
