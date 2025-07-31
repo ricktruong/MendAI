eafc4fe195f84818eefc8ad7d1b9e29db74b74b0
Rick Vu Truong <ricktruong@Ricks-MacBook-Pro.local>
2025-07-31 01:26:20 -0700

    Commented Backend Engine Routes

--- backend/engine/src/api/v0/routes.py
+++ backend/engine/src/api/v0/routes.py
@@ -3,4 +3,27 @@ from fastapi import APIRouter
 from .endpoints import *
 
 api_router = APIRouter(prefix="/v0")
-# include all the endpoints
+
+# TODO: include all the endpoints
+# Frontend -> Backend Engine endpoints
+# 1. Login Page
+# I. Doctor Account Login Request
+
+# 2. Dashboard Page
+# I. Select Patient Request
+# II. Logout Request?
+
+# 3. Chat Page
+# I.User Message Request
+
+
+# Frontend <- Backend Engine endpoints
+
+# Backend Engine -> Patient Data Service endpoints
+
+# Backend Engine <- Patient Data Service endpoints
+
+# Backend Engine -> LLM Service endpoints
+
+# Backend Engine <- LLM Service endpoints
+
